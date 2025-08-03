d1d279463501046b120d2c3f7b7b42273efcf1f2
AgungDH <agungdh@live.com>
2025-08-03 15:55:43 +0700

    feat(apollo): integrate GraphQL support and update project configs
    - Configure `angular.json` with `fileReplacements` for `environment.development.ts`
    - Create `src/environments/` directory for environment-specific files
    - Add `provideApollo` in `src/app.config.ts` using `HttpLink` and `InMemoryCache`
    - Install `react`, `react-dom`, and `scheduler` dependencies
    - Update `tsconfig.json` libs to include `dom` and `esnext.asynciterable`

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,16 +1,30 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, inject } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
+import { provideApollo } from 'apollo-angular';
+import { InMemoryCache } from '@apollo/client';
+import { environment } from './environments/environment';
+import { HttpLink } from 'apollo-angular/http';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
         provideHttpClient(withFetch()),
         provideAnimationsAsync(),
-        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
+        provideApollo(() => {
+            return {
+                link: inject(HttpLink).create({ uri: environment.graphqlUri }),
+                cache: new InMemoryCache(),
+                defaultOptions: {
+                    watchQuery: { fetchPolicy: 'network-only' },
+                    query: { fetchPolicy: 'network-only' },
+                },
+            };
+        }),
     ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://api.agungdh.my.id',
+    graphqlUri: 'https://api.agungdh.my.id/graphql',
+};
